43fc4e8539ef1faceb6cc67e301460f53e58f7f5
shaw.zheng <shaw.zheng@vip.51.ca>
2022-11-28 18:40:05 +0800

    add dPrint

---
+++ Sources/MyFirstLibrary/File.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by zchao on 28/11/2022.
+//
+
+import Foundation
+
+class DebugPrint {
+    static func dPrint(arg: Any) {
+        print("\(Date())--\(arg))")
+    }
+}
