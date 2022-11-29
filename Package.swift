c870194fd6dbb73398be095b3bd8e29653807604
shaw.zheng <shaw.zheng@vip.51.ca>
2022-11-29 15:08:47 +0800

    package

--- Package.swift
+++ Package.swift
@@ -21,8 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MyFirstLibrary",
-            dependencies: [],
-            path: "Sources/MyFirstLibrary"),
+            dependencies: []),
         .testTarget(
             name: "MyFirstLibraryTests",
             dependencies: ["MyFirstLibrary"]),
