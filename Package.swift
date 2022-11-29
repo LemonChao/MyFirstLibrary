8cb098ebd81813adc04f217fb6ced5ea31857f3d
shaw.zheng <shaw.zheng@vip.51.ca>
2022-11-29 14:52:21 +0800

    add path

--- Package.swift
+++ Package.swift
@@ -21,7 +21,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MyFirstLibrary",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources/MyFirstLibrary"),
         .testTarget(
             name: "MyFirstLibraryTests",
             dependencies: ["MyFirstLibrary"]),
